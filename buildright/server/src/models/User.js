7e32a3d385efe3a4bbb5bfa926436eca248e2c51
jinx71 <karim.sazed@gmail.com>
2021-02-16 19:23:00 +0000

    feat: define User Mongoose model

---
+++ buildright/server/src/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address'],
+    },
+    // select:false → the hash is never returned by default queries.
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'staff'],
+      default: 'admin',
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash the password whenever it is set or changed.
+userSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  return next();
+});
+
+// Compare a plaintext candidate against the stored hash.
+userSchema.methods.matchPassword = function matchPassword(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
